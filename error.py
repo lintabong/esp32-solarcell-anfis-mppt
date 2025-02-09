118182b5bff1109e27762a4ce7d39f7f0e682719
lintang <lintang.bb@gmail.com>
2025-02-09 22:50:39 +0700

    new

--- error.py
+++ error.py
@@ -14,16 +14,17 @@ mean_value = np.mean(values)
 
 # Mean Absolute Error (MAE)
 mae = np.mean([abs(x - mean_value) for x in values])
+mae_percent = (mae / mean_value) * 100
 
 # Mean Squared Error (MSE)
 mse = np.mean([(x - mean_value) ** 2 for x in values])
 
 # Standard Deviation (STD)
 std = np.std(values)
+std_percent = (std / mean_value) * 100
 
 # Menampilkan hasil
-# print(f"Data dari kolom C (baris 2-97): {values}")
 print(f"Mean: {mean_value:.8f}")
-print(f"Mean Absolute Error (MAE): {mae:.8f}")
+print(f"Mean Absolute Error (MAE): {mae:.8f} ({mae_percent:.2f}%)")
 print(f"Mean Squared Error (MSE): {mse:.8f}")
-print(f"Standard Deviation (STD): {std:.8f}")
+print(f"Standard Deviation (STD): {std:.8f} ({std_percent:.2f}%)")
